f7e5cc0469de30d4852b135ea33712fec05e34bf
Steve Pothier <pothiers@gmail.com>
2020-11-17 15:45:57 -0700

    wip

--- helpers/api.py
+++ helpers/api.py
@@ -1,33 +1,76 @@
 import requests
+from urllib.parse import urlencode
+from enum import Enum,auto
+
 
 # TODO:
 #   API should return seperate versions for SIA and ADS.
 #   Validate response from requests.  PPrint error if not success.
-#   Generalized: LIMIT, error handling
+#   Generalize: error handling
 #   Always JSON output (or pandas dataframe? or VOTABLE?)
 #   ENUM for File, Hdu
+#   ads search, HEADER in response should give full URL for 'endpoint'
+#   Authentication.
+#   VERBOSE mode (for debugging)
 # 
 #   Cache calls to relatively static content:
 #      cat_lists, version, *_*_fields, *adoc,
-#     
+#
+
+class Rec(Enum):
+    File = auto()
+    Hdu = auto()
 
 class AdaApi():
     """Astro Data Archive"""
     expected_version = 5.0
 
-    def __init__(self, url='https://astroarchive.noao.edu'):
+    def __init__(self,
+                 url='https://astroarchive.noao.edu',  verbose=False,
+                 username=None,  password=None):
         self.apiurl = f'{url}/api'
         self.adsurl = f'{url}/api/adv_search'
         self.siaurl = f'{url}/api/sia'
-        self.default_limit = 1000 # @@@
         self.categoricals = None
         self.version = None
+        self.verbose = verbose
+        if username is not None:
+            res = requests.post(f'{apiurl}/get_token/',
+                                json=dict(email=username, password=password))
+            if res.status_code == 200:
+                self.token = res.json()
+            else:
+                self.token = None
+                msg = (f'Credentials given '
+                       f'(username="{username}", password={password}) '
+                       f'could not be authenticated. Therefore, you will '
+                       f'only be allowed to retrieve PUBLIC files. '
+                       f'You can still get any metadata.' )
+                raise Exception(msg)
+            
+    def search(self, jspec, limit=False, format='json'):
+        # VALIDATE params @@@
+        qstr = urlencode(dict(limit=None if limit is None else (limit or self.limit),
+                              format=format))
+        t = 'h' if self.type == Rec.Hdu else 'f'
+        url = f'{self.adsurl}/{t}asearch/?{qstr}'
+        if self.verbose:
+            print(f'Search invoking "{url}" with: {jspec}')
+        res = requests.post(url, json=jspec)
+        return(res.json())
+
+    def vosearch(self, ra, dec, size, limit=100):
+        t = 'hdu' if self.type == Rec.Hdu else 'img'
+        qstr = urlencode(dict(POS=f'{ra},{dec}',SIZE=size))
+        url = f'{self.siaurl}/vo{t}?'
+        res = requests.get(url)
+        return(res.content.decode("utf-8"))
 
     def check_version(self):
         """Insure this library in consistent with the API version."""
         res = requests.get(f"{self.apiurl}​/version​/")
         return(False)
-    
+
     def get_categoricals(self):
         if self.categoricals is None:
             url = f'{self.adsurl}/cat_lists/'
@@ -37,43 +80,65 @@ class AdaApi():
 
     def get_aux_fields(self, instrument, proctype):
         # @@@ VALIDATE instrument, proctype, type
-        url = f'{self.adsurl}/aux_{self.type}_fields/{instrument}/{proctype}/'
+        t = 'hdu' if self.type == Rec.Hdu else 'file'
+        url = f'{self.adsurl}/aux_{t}_fields/{instrument}/{proctype}/'
         res = requests.get(url)
         print(f"url={url}; res={res}; content={res.content}")
         return(res.json())
 
     def get_core_fields(self):
+        t = 'hdu' if self.type == Rec.Hdu else 'file'
         # @@@ VALIDATE instrument, proctype, type
-        res = requests.get(f'{self.adsurl}/core_{self.type}_fields/')
+        res = requests.get(f'{self.adsurl}/core_{t}_fields/')
         return(res.json())
-
-    def search(self, jspec):
-        t = 'h' if self.type=='hdu' else 'f'
-        res = requests.post(f'{self.adsurl}/{t}asearch/', json=jspec)
-        return(res.json())
-
-    def vosearch(self, ra, dec, size, limit=False):
-        t = 'hdu' if self.type=='hdu' else 'img'
-        if not limit:
-            limit = self.default_limit 
-        res = requests.post(f'{self.siaurl}/vo{t}?POS={ra},{dec}&SIZE={size}&limit={limit}')
-        return(res.json())        
 
         
 class FitsFile(AdaApi):
-    def __init__(self, url='https://astroarchive.noao.edu'):
-        AdaApi.__init__(self, url=url)
-        self.type = 'file'
-        
+    def __init__(self, 
+                 url='https://astroarchive.noao.edu',
+                 verbose=False,
+                 limit=10,
+                 username=None,  password=None):
+        super().__init__(url=url.rstrip('/'), verbose=verbose,
+                         username=username, password=password)
+        self.type = Rec.File
+        self.limit = limit
+
+    def retrieve(self, fileid, hdu=None):
+        # VALIDATE params @@@
+        qparams = '' if hdu is None else f'/?hdu={hdu}'
+        url = f'{self.apiurl}/retrieve/{fileid}/{qparams}'
+        if self.token is None:
+            res = requests.get(url)
+        else:
+            res = requests.get(url, headers=dict(Authorization=self.token))
+        return res
 
 class FitsHdu(AdaApi):
-    def __init__(self, url='https://astroarchive.noao.edu'):
-        super().__init__(self, url=url)
-        self.type = 'hdu'
-
+    def __init__(self, 
+                 url='https://astroarchive.noao.edu',
+                 limit=20,
+                 verbose=False,
+                 username=None,  password=None):
+        super().__init__(url=url.rstrip('/'), verbose=verbose,
+                         username=username, password=password)
+        self.type = Rec.Hdu
+        self.limit = limit
 
 ##############################################################################        
 
+
+#@@@ # POST
+#@@@ def get_token​():
+#@@@     """Request an authorization token."""
+#@@@     url="/api​/get_token​/"
+#@@@
+#@@@ # GET
+#@@@ def retrieve​(md5):
+#@@@     """Download one FITS file from the Archive."""
+#@@@     url="/api​/retrieve​/{md5}​/"
+#@@@     pass
+#@@@ 
 
 #! # GET
 #! def vohdu():
@@ -147,10 +212,6 @@ class FitsHdu(AdaApi):
 #! ​    url="/api​/fields​/"
 #!     pass
 
-#@@@ # POST
-#@@@ def get_token​():
-#@@@     """Request an authorization token."""
-#@@@     url="/api​/get_token​/"
 
 #@@@ # GET
 #@@@ def header​(md5):  # @@@  HTML
@@ -162,12 +223,6 @@ class FitsHdu(AdaApi):
 #@@@ def object_lookup​():
 #@@@     """Retrieve the RA,DEC coordinates for a given object by name."""
 #@@@     url="/api​/object-lookup​/"
-#@@@     pass
-#@@@ 
-#@@@ # GET
-#@@@ def retrieve​(md5):
-#@@@     """Download one FITS file from the Archive."""
-#@@@     url="/api​/retrieve​/{md5}​/"
 #@@@     pass
 #@@@ 
 #!# GET
---
+++ tests/test_search.py
@@ -0,0 +1,32 @@
+import helpers.api
+from pprint import pformat
+
+fapi = helpers.api.FitsFile(url='http://marsnat1.pat.dm.noao.edu:8000/',
+                            verbose=True)
+
+res = fapi.search({"outfields": ["archive_filename"], "search":[]}, limit=5)
+# Search invoking "http://marsnat1.pat.dm.noao.edu:8000/api/adv_search/fasearch/?limit=5&format=json" with: {'outfields': ['archive_filename'], 'search': []}
+
+print(f'Result={pformat(res)}')
+
+# Result=[{'HEADER': {'archive_filename': 'string'},
+#   'META': {'endpoint': 'adv_search/fasearch'},
+#   'PARAMETERS': {'format': 'json',
+#                  'json_payload': {'outfields': ['archive_filename'],
+#                                   'search': []},
+#                  'last': 5,
+#                  'limit': 5,
+#                  'offset': 0,
+#                  'oldest': None,
+#                  'previd': None},
+#   'RESULTS': {'COUNT': 5, 'MORE': True}},
+#  {'archive_filename': '/net/archive/mtn/20181129/ct4m/2012B-0001/c4d_181130_010546_ori.fits.fz'},
+#  {'archive_filename': '/net/archive/mtn/20170307/kp4m/2016A-0453/k4m_170308_045842_ori.fits.fz'},
+#  {'archive_filename': '/net/archive/mtn/20181205/ct4m/2012B-0001/c4d_181205_224438_zri.fits.fz'},
+#  {'archive_filename': '/net/archive/mtn/20150413/bok23m/2015A-0801/ksb_150414_104425_ori.fits.fz'},
+#  {'archive_filename': '/net/archive/mtn/20180328/ct4m/2018A-0159/c4d_180329_035427_ori.fits.fz'}]
+
+def test_search():
+    res = fapi.search({"outfields": ["archive_filename"], "search":[]}, limit=5)
+    assert len(res) == 6
+    
