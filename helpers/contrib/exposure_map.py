36bd12db57f8fafb93b6f530403a3344a2b0cddd
Steve Pothier <pothiers@gmail.com>
2020-12-04 07:47:41 -0700

    exposure_map script works

--- helpers/contrib/exposure_map.py
+++ helpers/contrib/exposure_map.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
+# EXAMPLES:
+#   helpers/contrib/exposure_map.py -v --apiurl "http://marsnat1.pat.dm.noao.edu:8000/"
 
+# Python library
 import sys
+import argparse
+import copy
+from pprint import pprint as pp  # pretty print
 import pandas as pd
+import math
+import json
+# External packages
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
-import math
 import requests
-import json
 import healpy as hp
-from pprint import pprint as pp  # pretty print
+# Local Packages
+import helpers.api
 
 ##############################################################################
 # functions to order the vertices of HDU corners in counter-clockwise
@@ -72,7 +80,7 @@ def sort_radec(ra,dec):
 
 ##############################################################################
 
-def gen_exposure_map(fapi,hapi):
+def gen_exposure_map(fapi,hapi, verbose=False):
     jj = {"outfields" : ["md5sum", "AIRMASS", "G-TRANSP"],
           "search" : [
               ["instrument", "decam"],
@@ -82,8 +90,13 @@ def gen_exposure_map(fapi,hapi):
               ["proposal", "2012B-0001"],
               ["ifilter", "r DECam", "contains"]
           ]}
-    res = fapi.search(jj, limit=500000)
-    dff = pd.read_json(json.dumps(res[1:]))
+    if verbose:
+        print('Get AIRMASS and G-TRANSP for DECam files with selected filter.')
+    info, rows = fapi.search(jj, limit=500000)
+    dff = pd.DataFrame(rows)
+    if verbose:
+        print(f"Found {info['RESULTS']['COUNT']} files")
+        
 
     # Now we'll get the information needed from the individual HDUs.
     # This will take longer, because the number of HDUs is nearly two
@@ -115,9 +128,13 @@ def gen_exposure_map(fapi,hapi):
               ["fitsfile__ifilter", "r DECam", "contains"]
           ]}
     
-    res = hapi.search(jj, limit=5000000)
-    df2 = pd.read_json(json.dumps(res[1:]))
-
+    if verbose:
+        print('Get corner coordinates, FWHM, and AVSKY of HDUs.')
+    info, rows = hapi.search(jj, limit=5000000)
+    df2 = pd.DataFrame(rows)
+    if verbose:
+        print(f"Found {info['RESULTS']['COUNT']} HDUs")
+    
     dfm = pd.merge(dff,df2,left_on='md5sum',right_on='fitsfile')
     dfmc = dfm.dropna()
 
@@ -125,27 +142,42 @@ def gen_exposure_map(fapi,hapi):
     sky = dfmc['AVSKY']/dfmc['fitsfile__exposure'] # sky rate
     tau = dfmc['G-TRANSP']**2/(dfmc['FWHM']*apix/0.9)**2/(sky/3.)
 
+    if verbose:
+        print(f"Plot tau histogram")
     a = plt.hist(tau,bins=200,range=(0,1))
     plt.xlabel('tau')
 
     tau_trim = np.clip(tau,0,1) # tau should be between 0 and 1
 
-    # Making the depth map <a id="depth" />
-
-
+    ########################
+    # Making the depth map
+    #
+    # NOTE: Some of the complexity of the following could be reduced by using
+    # the bounding box of HDU instead of corners. Not as accurate but bbox is
+    # ordered. Use ra_min, ra_max, dec_min, dec_max
+    #
+    # Now that we have the needed quantities, we can begin to make our depth map. Our procedure will be to create a Healpix map, use the coordinates of the HDU corners to identify which healpixels are spanned by each HDU, and add the value of 𝜏
+    # 
+    # for those HDUs to the appropriate Healpixels. A couple of notes:
+    # 
+    #     Because the corners of the HDUs aren't guaranteed to go clockwise, or counter-clockwise, around the HDU, we might not be defining convex polygons when we do the healpixel mapping. We'll need to order the corners so that they go in one direction, and don't jump an HDU along its diagonal
+    #     We'll need to loop over all of the HDUs one at a time, which can be slow. Parallel processing might help here.
+    
     # Pull out the HDU corners
+    
     ratab = [np.array([row[0],row[1],row[2],row[3]])
              for row in dfmc[['COR1RA1','COR2RA1','COR3RA1','COR4RA1']].values]
     dectab = [np.array([row[0],row[1],row[2],row[3]])
               for row in dfmc[['COR1DEC1','COR2DEC1','COR3DEC1','COR4DEC1']].values]
 
-
-    adectab_s = [sort_radec(ra1,dec1) for ra1,dec1 in zip(ratab,dectab)]
+    radectab_s = [sort_radec(ra1,dec1) for ra1,dec1 in zip(ratab,dectab)]
     vectab = [hp.ang2vec(ra1,dec1,lonlat=True) for ra1, dec1 in radectab_s]    
+
 
     # Define the Healpix map
     nside = 4096
-    print('Resolution is {:5.2f} arcmin'.format(hp.nside2resol(nside,arcmin=True)))
+    if verbose:
+        print(f'Resolution is {hp.nside2resol(nside,arcmin=True):5.2f} arcmin.')
     map = np.zeros(hp.nside2npix(nside)) # raw exposure map
     tmap = map.copy() # teff map
 
@@ -159,11 +191,16 @@ def gen_exposure_map(fapi,hapi):
             pass
 
     # Show the map
+    if verbose:
+        print(f"Show exposure map in Orthographic view")
+    cmap = copy.copy(matplotlib.cm.get_cmap("inferno"))
     fig = plt.figure(figsize=(15,15))
-    hp.orthview(tmap,rot=(20,-30),fig=1,cmap='inferno',half_sky=True,min=0,max=1000)
+    hp.orthview(tmap,rot=(20,-30),fig=1,cmap=cmap,half_sky=True,min=0,max=1000)
 
     # Zooming in
-    hp.gnomview(tmap,reso=0.75,cmap='inferno',rot=(8,-44),min=0,max=1000)
+    if verbose:
+        print(f"Show exposure map in Gnomonic view")
+    hp.gnomview(tmap,reso=0.75,cmap=cmap,rot=(8,-44),min=0,max=1000)
     
     return tmap 
 
@@ -175,16 +212,22 @@ def main():
     parser = argparse.ArgumentParser(
         #version='1.0.0',
         description='Generate exposure map for a survey (from: Knut Olsen)',
-        epilog='EXAMPLE: %(prog)s a b"'
+        epilog='EXAMPLE: \n\t"%(prog)s -v"'
         )
-    parser.add_argument('--outdir',
-                        help='Directory to download files into. (must exist)' )
+    parser.add_argument('--apiurl',  help='URL of Archive API service',
+                        default='https://astroarchive.noao.edu/')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help=('Tell what is going on'))
+
     args = parser.parse_args()
 
-    fapi =  helpers.api.FitsFile()
-    hapi =  helpers.api.Hdu()
+    fapi =  helpers.api.FitsFile(args.apiurl)
+    hapi =  helpers.api.FitsHdu(args.apiurl)
 
-    map = gen_exposure_map(fapi,hapi)
+    if args.verbose:
+        print(f'Using API server at {args.apiurl}')
+    map = gen_exposure_map(fapi,hapi, verbose=args.verbose)
+    plt.show()
 
 if __name__ == '__main__':
     main()
